b88af822158bfe839cc8b9732790d07c495a2323
NguyenHoangThanhAn <skyannguyenhoangthanh@gmail.com>
2026-01-31 08:58:16 +0700

    coding networking package

---
+++ ComposeKMPProject1/ComposeKMPProject1/composeApp/src/commonMain/kotlin/org/nhta/nhta_kmp1/core/data/network/RemoteBookDataSource.kt
@@ -0,0 +1,15 @@
+package org.nhta.nhta_kmp1.core.data.network
+
+import org.nhta.nhta_kmp1.core.data.dto.BookWorkDto
+import org.nhta.nhta_kmp1.core.data.dto.SearchResponseDto
+import org.nhta.nhta_kmp1.core.domain.DataError
+import org.nhta.nhta_kmp1.core.domain.Result
+
+interface RemoteBookDataSource {
+    suspend fun searchBooks(
+        query: String,
+        resultLimit: Int? = null
+    ): Result<SearchResponseDto, DataError.Remote>
+
+    suspend fun getBookDetails(bookWorkId: String): Result<BookWorkDto, DataError.Remote>
+}
