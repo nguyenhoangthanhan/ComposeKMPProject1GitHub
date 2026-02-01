b81bdb4e8e5a1efeda9287d71c8dc6e344b300d3
NguyenHoangThanhAn <skyannguyenhoangthanh@gmail.com>
2026-02-01 13:32:01 +0700

    coding repositories

---
+++ ComposeKMPProject1/ComposeKMPProject1/composeApp/src/commonMain/kotlin/org/nhta/nhta_kmp1/core/data/DefaultBookRepository.kt
@@ -0,0 +1,69 @@
+package org.nhta.nhta_kmp1.core.data
+
+import androidx.sqlite.SQLiteException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import org.nhta.nhta_kmp1.book.domain.Book
+import org.nhta.nhta_kmp1.core.data.database.FavoriteBookDao
+import org.nhta.nhta_kmp1.core.data.mappers.toBook
+import org.nhta.nhta_kmp1.core.data.mappers.toBookEntity
+import org.nhta.nhta_kmp1.core.data.network.RemoteBookDataSource
+import org.nhta.nhta_kmp1.core.domain.BookRepository
+import org.nhta.nhta_kmp1.core.domain.DataError
+import org.nhta.nhta_kmp1.core.domain.EmptyResult
+import org.nhta.nhta_kmp1.core.domain.map
+import org.nhta.nhta_kmp1.core.domain.Result
+
+class DefaultBookRepository(
+    private val remoteBookDataSource: RemoteBookDataSource,
+    private val favoriteBookDao: FavoriteBookDao
+): BookRepository {
+    override suspend fun searchBooks(query: String): Result<List<Book>, DataError.Remote> {
+        return remoteBookDataSource
+            .searchBooks(query)
+            .map { dto ->
+                dto.results.map { it.toBook() }
+            }
+    }
+
+    override suspend fun getBookDescription(bookId: String): Result<String?, DataError> {
+        val localResult = favoriteBookDao.getFavoriteBook(bookId)
+
+        return if(localResult == null) {
+            remoteBookDataSource
+                .getBookDetails(bookId)
+                .map { it.description }
+        } else {
+            Result.Success(localResult.description)
+        }
+    }
+
+    override fun getFavoriteBooks(): Flow<List<Book>> {
+        return favoriteBookDao
+            .getFavoriteBooks()
+            .map { bookEntities ->
+                bookEntities.map { it.toBook() }
+            }
+    }
+
+    override fun isBookFavorite(id: String): Flow<Boolean> {
+        return favoriteBookDao
+            .getFavoriteBooks()
+            .map { bookEntities ->
+                bookEntities.any { it.id == id }
+            }
+    }
+
+    override suspend fun markAsFavorite(book: Book): EmptyResult<DataError.Local> {
+        return try {
+            favoriteBookDao.upsert(book.toBookEntity())
+            Result.Success(Unit)
+        } catch(e: SQLiteException) {
+            Result.Error(DataError.Local.DISK_FULL)
+        }
+    }
+
+    override suspend fun deleteFromFavorites(id: String) {
+        favoriteBookDao.deleteFavoriteBook(id)
+    }
+}
---
+++ ComposeKMPProject1/ComposeKMPProject1/composeApp/src/commonMain/kotlin/org/nhta/nhta_kmp1/core/domain/BookRepository.kt
@@ -0,0 +1,14 @@
+package org.nhta.nhta_kmp1.core.domain
+
+import kotlinx.coroutines.flow.Flow
+import org.nhta.nhta_kmp1.book.domain.Book
+
+interface BookRepository {
+    suspend fun searchBooks(query: String): Result<List<Book>, DataError.Remote>
+    suspend fun getBookDescription(bookId: String): Result<String?, DataError>
+
+    fun getFavoriteBooks(): Flow<List<Book>>
+    fun isBookFavorite(id: String): Flow<Boolean>
+    suspend fun markAsFavorite(book: Book): EmptyResult<DataError.Local>
+    suspend fun deleteFromFavorites(id: String)
+}
