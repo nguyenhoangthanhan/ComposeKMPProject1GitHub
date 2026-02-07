ea910ca8edc190c68874d48b18006e2e04a4d383
NguyenHoangThanhAn <skyannguyenhoangthanh@gmail.com>
2026-02-08 00:36:32 +0700

    coding book list screen

--- ComposeKMPProject1/ComposeKMPProject1/composeApp/src/commonMain/kotlin/org/nhta/nhta_kmp1/book/presentation/book_list/BookListState.kt
+++ ComposeKMPProject1/ComposeKMPProject1/composeApp/src/commonMain/kotlin/org/nhta/nhta_kmp1/book/presentation/book_list/BookListState.kt
@@ -6,8 +6,8 @@ import org.nhta.nhta_kmp1.core.presentation.UiText
 data class BookListState(
     val searchQuery: String = "Kotlin",
     val searchResults: List<Book> = emptyList(),
-    val favoriteList: List<Book> = emptyList(),
-    val isLoading: Boolean = false,
+    val favoriteBooks: List<Book> = emptyList(),
+    val isLoading: Boolean = true,
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null
 )
