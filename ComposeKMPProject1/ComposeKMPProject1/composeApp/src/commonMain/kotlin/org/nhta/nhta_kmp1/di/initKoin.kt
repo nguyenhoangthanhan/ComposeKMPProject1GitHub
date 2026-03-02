3de373e423eed0e09157e26e1017732448bafa28
NguyenHoangThanhAn <skyannguyenhoangthanh@gmail.com>
2026-03-02 19:23:29 +0700

    coding initKoin

--- ComposeKMPProject1/ComposeKMPProject1/composeApp/src/commonMain/kotlin/org/nhta/nhta_kmp1/di/initKoin.kt
+++ ComposeKMPProject1/ComposeKMPProject1/composeApp/src/commonMain/kotlin/org/nhta/nhta_kmp1/di/initKoin.kt
@@ -1,11 +1,11 @@
-//package org.nhta.nhta_kmp1.di
-//
-//import org.koin.core.context.startKoin
-//import org.koin.dsl.KoinAppDeclaration
-//
-//fun initKoin(config: KoinAppDeclaration? = null) {
-//    startKoin {
-//        config?.invoke(this)
-//        modules(sharedModule, platformModule)
-//    }
-//}
+package org.nhta.nhta_kmp1.di
+
+import org.koin.core.context.startKoin
+import org.koin.dsl.KoinAppDeclaration
+
+fun initKoin(config: KoinAppDeclaration? = null) {
+    startKoin {
+        config?.invoke(this)
+        modules(sharedModule, platformModule)
+    }
+}
