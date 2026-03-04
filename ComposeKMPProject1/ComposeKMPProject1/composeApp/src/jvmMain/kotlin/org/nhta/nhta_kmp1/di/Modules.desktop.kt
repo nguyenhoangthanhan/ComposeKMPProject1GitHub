50cb8ce516c1480b8e6ec0a9162a917b5e80c7f8
NguyenHoangThanhAn <skyannguyenhoangthanh@gmail.com>
2026-03-04 23:42:58 +0700

    coding jvm main

--- ComposeKMPProject1/ComposeKMPProject1/composeApp/src/jvmMain/kotlin/org/nhta/nhta_kmp1/di/Modules.desktop.kt
+++ ComposeKMPProject1/ComposeKMPProject1/composeApp/src/jvmMain/kotlin/org/nhta/nhta_kmp1/di/Modules.desktop.kt
@@ -4,6 +4,7 @@ import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import org.nhta.nhta_kmp1.book.data.database.DatabaseFactory
 
 actual val platformModule: Module
     get() = module {
